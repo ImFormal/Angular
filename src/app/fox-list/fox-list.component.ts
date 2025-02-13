a63605d5f9b71c7f4084de38cd6f73f16794d533
ImFormal <thubert.maxime.pro@gmail.com>
2025-02-13 11:40:07 +0100

    son update

--- src/app/fox-list/fox-list.component.ts
+++ src/app/fox-list/fox-list.component.ts
@@ -42,8 +42,7 @@ export class FoxListComponent {
 
   enableAudioPlayback = () => {
     if (!this.audio) {
-      const basePath = window.location.hostname === 'localhost' ? '' : '/Angular';
-      this.audio = new Audio(`${basePath}/assets/fox-song.mp3`);
+      this.audio = new Audio(`/assets/fox-song.mp3`);
       this.audio.volume = 0.5;
       this.audio.loop = true;
       this.audio.play().catch(error => console.error("Lecture audio bloquée", error));
